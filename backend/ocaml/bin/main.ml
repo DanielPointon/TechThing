0662d9fa34323bfff1173926fbda02bd4309d66e
Locky <bakerlochlann@gmail.com>
2022-08-19 12:43:49 +0100

    uh nothing

--- backend/ocaml/bin/main.ml
+++ backend/ocaml/bin/main.ml
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-let () = print_endline "Hello, World!"
-=======
 let () =
     print_endline "trololol"
-<<<<<<< HEAD
->>>>>>> 0cce775 (blep)
-=======
->>>>>>> 0cce775f541f7d48abe6af3ebff1d9f64fbb8177
